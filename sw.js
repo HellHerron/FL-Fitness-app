e19c273183cfeebd38b2c0863f5e9e683aa70f25
HellHerron <green1herron@gmail.com>
2026-02-04 16:07:18 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Fitness Log service worker (vibrant v2) - cache bust
-const CACHE="fitness-log-vibrant-v2_3-20260204";
+const CACHE="fitness-log-vibrant-v2_4-20260204";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
